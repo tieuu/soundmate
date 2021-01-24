5e95fe4ca7cb11d8c64b3d970fb44488eef5c7d0
Tieu Linh Dong <linhdongtieu@gmail.com>
2021-01-24 21:06:15 +0900

    added presence validation for booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,5 +4,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates :status, inclusion: { in: STATUS }
+  validates :status, inclusion: { in: STATUS }, presence: true
 end
