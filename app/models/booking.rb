96e9857b5c3be18981afcc773dae8016d5eee4bf
Tieu Linh Dong <linhdongtieu@gmail.com>
2021-01-24 21:12:29 +0900

    more validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
-  STATUS = %w[pending confirmed cancelled]
+  STATUS = %w[pending confirmed cancelled finished]
   belongs_to :equipment
   belongs_to :user
   validates :start_date, presence: true
--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,8 +1,9 @@
 class Equipment < ApplicationRecord
+  CATEGORY = %w[brass guitar keyboard percussion strings woodwind equipment]
   belongs_to :user
   has_many :bookings
   validates :ad_name, presence: true
-  validates :category, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORY }
   validates :description, presence: true
   validates :active, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
