683bea163e72c502f3ba7e97889d9a372a7ae361
Tieu Linh Dong <linhdongtieu@gmail.com>
2021-01-28 20:32:37 +0900

    added bookings index to show 'my bookings'

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :equipment, only: [:index, :show, :new, :create] do
-    resources :bookings, only: [:new, :index, :create, :edit, :update]
+    resources :bookings, only: [:new, :create, :edit, :update]
   end
-  resources :bookings, only: [:show]
+  resources :bookings, only: [:show, :index]
 end
