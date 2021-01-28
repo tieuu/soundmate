b7839f117096d4246f24cfb93a88ac2d876f8de3
Vincent Msi <msika.vincent@gmail.com>
2021-01-28 21:33:40 +0900

    Home page second version

--- app/javascript/packs/components/background.js
+++ app/javascript/packs/components/background.js
@@ -1,9 +1,10 @@
-const changeColor = document.querySelector(".banner") => {
+const changeColor = () => {
+const banner = querySelector(".banner")
 
    let hue = 0;
     function changeBg() {
       if (hue >= 360) hue = 0;
-      document.banner.style.backgroundColor = "hsl(" + hue + ",80%,50%)";
+      banner.style.backgroundColor = "hsl(" + hue + ",80%,50%)";
       hue += 1;
       setTimeout(() => { changeBg() }, 150);
     };
